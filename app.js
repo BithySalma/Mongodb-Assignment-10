4d06dc16c7f6d485a25024cdeac1e5ca1a2e0c40
Salma Aktar Bithy <76905965+BithySalma@users.noreply.github.com>
2023-10-15 21:12:50 +0600

    updated

---
+++ app.js
@@ -0,0 +1,50 @@
+// Basic Packages Import
+const express = require('express');
+const app = new express();
+const bodyParser = require('body-parser');
+const router = require('./src/routes/api');
+// Security Middleware Packages Import
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const xssClean = require('xss-clean');
+const cors = require('cors');
+// Database Packages Import
+const mongoose = require('mongoose');
+
+// Imported Packages Use
+// Security ------------
+app.use(cors());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xssClean());
+app.use(hpp());
+
+// Body Parser ----------
+app.use(bodyParser.json());
+
+// Setup Request Rate Limit
+const limiter = rateLimit({WindowMs:15*60*100,max:3000});
+app.use(limiter);
+
+
+let URI = "mongodb://127.0.0.1:27017/StudentAuth";
+
+mongoose.connect(URI).then(() => console.log("Connected Successfully"))
+.catch((err) => {
+  console.error(err);
+});
+
+// Routing Implement
+app.use('/api/v1',router);
+
+// Undefine Route
+app.use('*',(req,res)=>{
+    res.status(404).json({status:'fail',data:'Not Found'});
+});
+app.listen(5000,()=>{
+    console.log("App running on 5000");
+});
+
+module.exports = app;
